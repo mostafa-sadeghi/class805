7108769cc5e7c304ebeef1f7464c62d3d0f0c239
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-10-31 18:05:37 +0330

    first commit

---
+++ 805.py
@@ -0,0 +1,92 @@
+# name1 = 12
+# name2 = 'a'
+# name3 = 'reza'
+
+# number = 1
+
+
+# print(type(number))
+# print(type(name1))
+# print(type(name2))
+# print(type(name3))
+
+# message = """hello world
+# How are you?"""
+# print(message)
+
+
+# message = '''He said, "Aren't can't shouldn't wouldn't."'''
+# print(message)
+# message = 'He said, "Aren\'t can\'t shouldn\'t wouldn\'t."'
+# print(message)
+
+# message = "he said \"hi\""
+# print(message)
+
+
+# name = "ali"
+# family = "rezaei"
+
+# # message = "hello %s%s" % (name, family)
+# message = f'hello {name} {family}'
+
+# print(message)
+
+# print('*' + '*' + '*')
+# print('*' * 3)
+
+# print(1 + '1')
+
+# x = 1.3
+
+# print(type(x))
+
+
+# shopping_list = "egg, apple, bread, milk, pen"
+# print(shopping_list[10])
+
+
+# shopping_list = ["egg", "apple", "bread", "milk", "pen"]
+# print("first item: ", shopping_list[0])
+# print("second item: ", shopping_list[1])
+# print("third item: ", shopping_list[2])
+
+
+# print("before change: ", shopping_list)
+# shopping_list[1] = "banana"
+# print("after change: ", shopping_list)
+
+# string = "hello"
+
+# new_str = string[:3]
+# print(new_str)
+
+
+# shopping_list = ["egg", "apple", "bread", "milk", "pen", "potato", "tomato"]
+
+# slice_list = shopping_list[0:3]
+# slice_list = shopping_list[:3]
+# slice_list = shopping_list[1:6:2]
+
+# print(slice_list)
+
+
+# persons = ['ali', 1, 2, "reza", "mohamad", "javad", "alireza"]
+# persons[1] = "tina"
+
+
+# print(persons[0])
+# print(persons[1])
+# print(persons[2])
+# print(persons[3])
+# print(persons[4])
+# print(persons[1:4])
+
+
+numbers = [1, 2, 3, 4]
+persons = ['a', 'b']
+
+my_list = [numbers, persons]
+
+print(my_list)
+# // todo how to access and print just 'a' from my_list

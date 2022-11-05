1f2ed95ccc0fc5e929b655a1ac510777bfd4563e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-05 18:58:26 +0330

    14ababn

---
+++ 14aban.py
@@ -0,0 +1,46 @@
+numbers = [1, 2, 3, 4, 5, 6, 7]
+
+# print(numbers[1:4])
+# print(numbers[:])
+# print(numbers[1:])
+# print(numbers[:5])
+# print(numbers[::])
+# print(numbers[::2])
+
+# even and odd numbers from 1 to 10
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print("odd numbers are : ", numbers[::2])
+# print("even numbers are : ", numbers[1::2])
+
+
+# string = 3 * '*'
+# print(string)
+# numbers = 3 * ['ali', 1]
+# numbers = ['ali', 1] + ['ali', 1] + ['ali', 1]
+# print(numbers)
+
+
+# numbers = (1, 2, 3, 4, 5, 6)
+# print(type(numbers))
+# numbers[3] = -20
+# print(numbers)
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# numbers[3] = -20
+# print(numbers)
+
+# numbers = (1, 2, 3, 4, 5, 6)
+
+# print(numbers[5])
+# print(numbers[2:5])
+
+
+favorite_sports = {
+    'radman': 'footbal',
+    'radin': 'baseball',
+    'kian': 'basketball'
+}
+
+print(favorite_sports['radin'])
+print(favorite_sports['kian'])

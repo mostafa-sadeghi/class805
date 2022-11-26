7e4d561b376b2d9c23cfc305ed24f0b18bb2229f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-26 18:59:18 +0330

    3azar

--- aban_30.py
+++ aban_30.py
@@ -1,32 +1,33 @@
-# import turtle
-# import time
+import turtle
+import time
 
-# screen = turtle.Screen()
-# screen.bgcolor('orange')
-# t = turtle.Pen()
-# t.pensize(4)
-# t.speed('fast')
+screen = turtle.Screen()
+screen.bgcolor('orange')
+t = turtle.Pen()
+t.pensize(4)
+t.speed('fast')
+COLORS = ["red", "green", "blue", "black"]
+for j in range(12):
+    t.pencolor(COLORS[j % 4])
+    for i in range(4):
+        t.forward(100)
+        t.left(90)
+    # time.sleep(1.5)
+    t.left(30)
+    # time.sleep(1.5)
 
-# for j in range(12):
-#     for i in range(4):
-#         t.forward(100)
-#         t.left(90)
-#     time.sleep(1.5)
-#     t.left(30)
-#     time.sleep(1.5)
 
-
-# screen.exitonclick()
+screen.exitonclick()
 
 # تمرین : تغییر رنگ هر یک از مربع ها
 #
 
 # حلقه while
-people = []
+# people = []
 
-while True:
-    name = input('enter your name to add or "exit" to exit from the app: ')
-    if name == 'exit':
-        print(people)
-        exit()
-    people.append(name)
+# while True:
+#     name = input('enter your name to add or "exit" to exit from the app: ')
+#     if name == 'exit':
+#         print(people)
+#         exit()
+#     people.append(name)
---
+++ azar_5.py
@@ -0,0 +1,19 @@
+# exercise : با کمک حلقه for  100بار آن عدد را با خودش جمع نمائید.
+# exercise 2: برنامه ای بنویسید که با کمک حلقه for اعداد زوج کمتر از 1000 را نمایش دهد
+# exercise 3 : با کمک حلقه for برنامه ای بنویسید که یک عدد را گرفته و 1000 بار با عدد یک جمع نماید.
+
+
+# numbers = []
+# for i in range(1000, 2001):
+#     numbers.append(i)
+
+# print(numbers[10:71])
+numbers = []
+i = 1000
+while i < 2001:
+    numbers.append(i)
+    i = i + 1
+
+print(numbers[10:71])
+
+# exercise 4 : تمرین های 1 تا 3 را با حلقه while  انجام دهید.

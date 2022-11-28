2424fd6ceb98661ab198ba7eef712c90e5321f7f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-11-28 18:57:56 +0330

    7azar

---
+++ exercise_1.py
@@ -0,0 +1,36 @@
+# exercise 1:
+# x = int(input('enter a number> '))
+# print(x)
+# print(type(x))
+# output = x
+# for i in range(100):
+#     output = output + x
+
+# print(output)
+
+# exercise 2:
+# even_numbers = []
+# for i in range(1, 1001):
+#     if i % 2 == 0:
+#         even_numbers.append(i)
+
+# print(even_numbers)
+
+
+# exercise 3:
+# x = int(input('enter your number '))
+# for i in range(1000):
+#     x = x + 1
+# print(x)
+
+
+# exercise 3 with while loop
+
+x = int(input('enter your number '))
+
+i = 0
+
+while i < 1000:
+    x = x + 1
+    i = i + 1
+print(x)

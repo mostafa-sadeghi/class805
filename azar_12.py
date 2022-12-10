ed95c6cc2d199d6744f29c8e4d0bee2f163bcccf
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-10 18:57:31 +0330

    19azar

--- azar_12.py
+++ azar_12.py
@@ -16,14 +16,40 @@
 #     n = int(input('enter a number > '))
 #     numbers.append(n)
 
-numbers = list(range(0, 1000, 2))
-string = 'abc'
-s = string
-print(s)
-string_list = list(s)
-print(string_list)
+# numbers = list(range(0, 1000, 2))
+# string = 'abc'
+# s = string
+# print(s)
+# string_list = list(s)
+# print(string_list)
 
 # exercise:
 '''
 برنامه ای بنویسید که اسم پنج نفر را از ورودی بگیرد و در کنار هم نمایش دهد
 '''
+# string = ''
+
+# for i in range(5):
+#     name = input('enter your name> ')
+#     string += name + ' '  # string = string + name
+# print(string)
+
+# max value between five input numbers:
+# first solution
+# max_number = 0
+
+# for i in range(5):
+#     number = int(input('enter a number '))
+#     if number > max_number:
+#         max_number = number
+# print('max_number is', max_number)
+
+
+# second solution
+
+numbers = []
+for i in range(5):
+    number = int(input('enter a number> '))
+    numbers.append(number)
+max_number = max(numbers)
+print('max number is', max_number)

537f8d3010022068a8e028ec74fc19ded20bbdfc
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-29 19:58:19 +0330

    bajas

---
+++ 3.py
@@ -0,0 +1,24 @@
+'''
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+'''
+# print('* '*1)
+# print('* '*2)
+# print('* '*3)
+# print('* '*4)
+# print('* '*3)
+# print('* '*2)
+# print('* '*1)
+
+# for i in range(7):
+#     if i < 7/2:
+#         print('* '*(i+1))
+#     else:
+#         print(('* '*(7-i)))
+
+# while loop

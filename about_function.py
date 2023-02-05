ff5741c836e96805ce936522d98a3b3d56aa7d10
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-05 19:57:32 +0330

    sadeghi

---
+++ about_function.py
@@ -0,0 +1,29 @@
+# def my_function(n, family, cls_):
+#     print(f"hello {n} {family}")
+#     print(f"welcome to our {cls_} class")
+#     print(f"{cls_} from zero to hero")
+
+
+# name = input('enter a name> ')
+# family = input('enter a family> ')
+# class_ = "python"
+# my_function(name, family, class_)
+# my_function(cls_=class_, family=family, n=name)
+
+
+def add(number1, number2, number3):
+    return number1 + number2 + number3
+
+
+result = add(1, 2, 3)
+print(result)
+
+# exercise1
+# create three function:
+# sub
+# mul
+# div
+
+# exercise2 : تابعی بنویسید که نام، نام خانوادگی و سن را بعنوان پارامتر داشته باشدو
+#  اگر سن داده شده بزرگتر از 18 باشد پیغام زیر را برگرداند
+# your are adult.

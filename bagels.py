7b261503cd3293df41065a0a821a79075f8a1dea
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-19 20:00:36 +0330

    sadeghi

--- bagels.py
+++ bagels.py
@@ -6,8 +6,22 @@ MAX_GUESSES = 10
 
 
 def generate_secret_number():
-    secret_number = random.randrange(100, 1000)
-    return secret_number
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+    numbers = numbers[:3]
+    result = ''
+    for n in numbers:
+        result += n
+    return result
+
+
+def getHint(guess, secNumber):
+    if guess == secNumber:
+        return 'You got it!'
+
+    hints = []
+    for i in range(len(guess)):
+        if guess[i] == secNumber[i]:
 
 
 print(f'''Welcome to our bagels game.
@@ -17,6 +31,12 @@ Pico                        On digit is correct but not in the rigth postion.
 Fermi                       On digit is correct and in the rigth postion.
 Bagels                      Nothing is True.
 ''')
-
-sec_num = generate_secret_number()
-print(sec_num)
+while True:
+    sec_num = generate_secret_number()
+    print("secret number generated!!!!")
+    numGuesses = 1
+    while numGuesses <= MAX_GUESSES:
+        guess = ''
+        while len(guess) != NUM_DIGITS or not guess.isdecimal():
+            guess = input('> ')
+        hint = getHint(guess, sec_num)
